36e84f3354ff327e67b63e5c39a4d9c70629fdab
Hakob650 <hakob11meneshian@gmail.com>
2025-05-20 21:23:51 +0400

    Update task3.c

--- Bitwise/task3.c
+++ Bitwise/task3.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+void func(int num){
+    char hex_digits[]="0123456789ABCDEF";
+    char hex[50];
+    int i=0;
+    if(num==0){
+        printf("Hex: 0");
+        return;
+    }
+    while(num!=0){
+        hex[i]=hex_digits[num%16];
+        num/=16;
+        ++i;
+    }
+    printf("Hex: ");
+    for(int j=i-1;j>=0;--j){
+        printf("%c",hex[j]);
+    }printf("\n");
+}
+int main(){
+    int num;
+    printf("Enter number");
+    scanf("%d",&num);
+    func(num);
+    return 0;
+}

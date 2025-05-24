403042afbd942335e7e9df0112cc8ffd6785c93e
Hakob650 <hakob11meneshian@gmail.com>
2025-05-24 21:48:33 +0400

    Update task1.c

--- Files/task1/task1.c
+++ Files/task1/task1.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    FILE *fp;
+    char buf[100];
+    fp=fopen("example.txt","w");
+    if(fp==NULL){
+        perror("Failed to open file to write");
+        return 1;
+    }
+    fprintf(fp,"this is an example text in file");
+    fclose(fp);
+    fp=fopen("example.txt","r");
+    if(fp==NULL){
+        perror("Failed to open file to write");
+        return 1;
+    }
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
+        printf("%s",buf);
+    }
+    fclose(fp);
+    return 0;
+}

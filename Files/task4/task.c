0cca632e9abecf19791b8047e05ffb29512ac7ae
Hakob650 <hakob11meneshian@gmail.com>
2025-05-24 21:53:10 +0400

    Create task.c

---
+++ Files/task4/task.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    FILE *fp;
+    char *buf;
+    long offset=192;
+    size_t readsize=13;
+    fp=fopen("example.txt","r");
+    if(fp==NULL){
+        perror("Failed to open file to read");
+        return 1;
+    }
+    buf=(char*)malloc(readsize);
+    if(buf==NULL){
+        fclose(fp);
+        return 1;
+    }
+    for(int i=0;i<2;++i){
+        if(fseek(fp,offset,SEEK_SET)!=0){
+            free(buf);
+            fclose(fp);
+            return 1;
+        }
+        size_t byteread=fread(buf,1,readsize,fp);
+        buf[byteread]='\0';
+        printf("Read %d:%s\n",i+1,buf);
+    }
+    free(buf);
+    fclose(fp);
+    return 0;
+}
